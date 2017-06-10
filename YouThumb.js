584b58fb5edcc4acbe8d30d96a6fd6273d18b09d
Alexander Nevidlovskiy <nevidlovskyi@yandex.ru>
2017-06-10 23:58:26 +0700

    aspect ratio fix

--- YouThumb.js
+++ YouThumb.js
@@ -14,9 +14,9 @@
 
 (function ($, undefined) {
     $(function () {
-    
+
         'use strict';
-        
+
         var t = { // translates
                 show_thumb: { ru: "Показать миниатюру", en: "Show thumb" },
                 hide_thumb: { ru: "Скрыть миниатюру", en: "Hide thumb" },
@@ -29,13 +29,13 @@
             imgSrc,
             LANG = navigator.language,
             $body = $('body'),
-            bp_width = '24px',
-            bp_height = '18px',            
+            bp_width = '32px',
+            bp_height = '20px',
         endvar;
-        
+
         if(LANG == 'en-US') LANG = 'en';
         if(LANG == 'ru-RU') LANG = 'ru';
-          
+
         function ShowHideThumbnail(){ // when click on buttons or thumbnail
 
             var YouThumb = $('#YouThumb');
@@ -70,17 +70,17 @@
         }
 
         function SetButton(){ //if(document.getElementById("YouThumbButton")!==null) return;
-            
+
             //let params = (new URL(document.location)).searchParams;            let v = params.get('v');
-            
+
             var YouThumb = $('#YouThumb');
             if(YouThumb.length !== 0){
                 YouThumb.remove();
                 $('#movie_player').css('visibility','visible');
             }
-            
+
             if($('#YouThumbButton').length !== 0) { clearInterval(setButtonInterval); return; }
-            
+
             //if(isset(button.v)) return;
             try {
                 //imgSrc = $('link[itemprop="thumbnailUrl"]')[0].href;
@@ -93,7 +93,7 @@
                 return;
             }
             clearInterval(setButtonInterval);
-            //cl(imgSrc);            
+            //cl(imgSrc);
             //yt-uix-button yt-uix-button-hh-text
             $('<span><button id=YouThumbButton class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup  yt-uix-button-toggled yt-uix-post-anchor yt-uix-tooltip"><img id=YouThumbImage src="'+imgSrc+'" style="width:'+ bp_width +';height:'+ bp_height +'"/> '+ t.show_thumb[LANG] +'</button></span>').appendTo($('.like-button-renderer')[0]);
 
@@ -103,14 +103,14 @@
                  var YouThumbImg = document.createElement("img"); // creating our Thumb picture that we need to show
                   YouThumbImg.setAttribute("src",links);
                   if((YouThumbImg.naturalWidth==120 && YouThumbImg.naturalHeight==90) || YouThumbImg.naturalWidth==0) links = links.replace(/maxres/,'hq');
-           if(YouThumbExt.YouThumbImage!=links) YouThumbExt.YouThumbImage=links;*/           
+           if(YouThumbExt.YouThumbImage!=links) YouThumbExt.YouThumbImage=links;*/
         }
 
         function mocallback(mutationrecords){
             //if(/watch/i.test(location.href)) setTimeout(SetButton, 2000); // TODO launch every 200 msecs. while element link[itemprop="thumbnailUrl"] not be found, and no more than 50 times!
             setButtonInterval = setInterval(SetButton, 500); // TODO launch every 200 msecs. while element link[itemprop="thumbnailUrl"] not be found, and no more than 50 times!
         }
-        
+
             //cl(typeof document.querySelector('title').nodeType);
 
             //cl(document.getElementById('eow-title').nodeType);
