9fefb4297af792219e6967f3dc113cc046f48746
Alexander Nevidlovskiy <nevidlovskyi@yandex.ru>
2017-06-30 23:54:54 +0700

    fix

--- YouThumb.js
+++ YouThumb.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouThumb! YouTube thumbnails showing
 // @namespace    www.youtube.com/watch.youthumb
-// @version      1.2
+// @version      1.3
 // @license      GPLv2
 // @description  Show YouTube thumbnail picture by button, near likes buttons.
 // @author       zanygamer@gmail.com
@@ -18,16 +18,16 @@
         https://www.youtube.com/watch?v=y0OzPjlJHzM - only hqdefautl
         https://www.youtube.com/watch?v=ZckOGWOYfFA - WO maxres
         https://www.youtube.com/watch?v=8Zy9InnJ4-Y&index=11&list=PLzPivwyXljVV100GMKcQAekdThrN2z6DQ - Jump over playlist
-    
+
     TODO
         Show thumbs not only on watch page
 */
 
 (function ($, undefined) {
     $(function () {
-        
+
         'use strict';
-        
+
         var t = { // translates
             show_thumb: { ru: "Показать миниатюру", en: "Show thumb" },
             hide_thumb: { ru: "Скрыть миниатюру", en: "Hide thumb" },
@@ -49,21 +49,21 @@
         bp_height = '20px',
         widthes = [0, 0, 0],
         endvar;
-        
+
         if(LANG == 'en-US') LANG = 'en';
         if(LANG == 'ru-RU') LANG = 'ru';
-        
+
         function showThumbnail(){
 
             var YouThumb = $('#YouThumb');
             var playerApi = $('#player-api');
-            
+
             playerApi.prepend('<img title="'+ t.close_thumb[LANG] +'" id=YouThumb src='+ imgSrc +' style=position:absolute;z-index:999>');
             YouThumb = $('#YouThumb');
             YouThumb.on('click', ShowHideThumbnail);
-            
+
             var offval = 0;
-            
+
             if((YouThumb[0].naturalWidth / YouThumb[0].naturalHeight) > 1.4) { // keep aspect ratio
                 YouThumb.attr('width', playerApi[0].offsetWidth);
                 YouThumb.attr('height', playerApi[0].offsetHeight); //-25
@@ -72,66 +72,62 @@
                 YouThumb.attr('height', playerApi[0].offsetHeight);
                 offval = (playerApi[0].offsetWidth - (playerApi[0].offsetHeight + (playerApi[0].offsetHeight / 3))) / 2;
             }
-            
+
             YouThumb.css('left', parseInt(YouThumb.css('left'))+ offval +'px');
             $('#YouThumbStatus').html(t.hide_thumb[LANG]);
-            $('#movie_player').css('visibility','hidden');           
+            $('#movie_player').css('visibility','hidden');
         }
-        
+
         function hideThumbnail(){
             $('#YouThumbStatus').html(t.show_thumb[LANG]);
             YouThumb.remove();
-            $('#movie_player').css('visibility','visible');            
+            $('#movie_player').css('visibility','visible');
         }
-        
+
         function ShowHideThumbnail(event){ // when click on buttons or thumbnail
-            
+
             var YouThumb = $('#YouThumb');
 
             if(event){ imgSrc = event.target.nodeName == 'SPAN' ? defaultImgSrc : event.target.src; }
-            
+
             if(YouThumb.length !== 0 && event && event.target.nodeName == 'SPAN'){ hideThumbnail(); return; }
-            
+
             if(YouThumb.length == 0 || (YouThumb.length !== 0 && $('#YouThumb')[0].src != imgSrc)){
 
                 if(YouThumb.length !== 0) YouThumb.remove();
                 showThumbnail();
-                
+
             } else hideThumbnail();
         }
-        
+
         function getWidths(){
-            count++; if(count > maxCount){ cl('LIMIT'); return; }            
+            count++; if(count > maxCount){ cl('LIMIT'); return; }
             for(let i = 0; i < images.length; i++){
                 let img = document.getElementById('YouThumb_'+ images[i] +'_imgSrc');
                 if(!img){ setTimeout(getWidths, 500); return; }
                 widthes[i] = img.naturalWidth;
             };
-            
+
             let to_insert = '';
             for(let i = 0; i < widthes.length; i++){
                 if(widthes[i] == 0){ setTimeout(getWidths, 500); return;}
                 if(widthes[i] > 120){
-                    if(!defaultImgSrc) defaultImgSrc = imgBase+images[i];
-                    cl(defaultImgSrc);
+                    if(!defaultImgSrc) defaultImgSrc = imgBase+images[i];//                    cl(defaultImgSrc);
                     to_insert+= ' <img class=YouThumbButtonImage src="'+ imgBase+images[i] +'" style="width:'+ bp_width +';height:'+ bp_height +'"/>';
                 }
             }
             $('#YouThumbButton').append(to_insert);
         }
-        
+
         function SetButton(){
-            
+
             var YouThumb = $('#YouThumb');
-            var YouThumbButton = $('.YouThumbButton');
-            
-            if(YouThumb.length !== 0){// YouThumb.remove();
-                ShowHideThumbnail();
-                $('#movie_player').css('visibility','visible');
-            }
-            
+            var YouThumbButton = $('#YouThumbButton');
+
+            if(YouThumb.length !== 0) hideThumbnail(); // YouThumb.remove();                ShowHideThumbnail();
+                //$('#movie_player').css('visibility','visible');
             if(YouThumbButton.length !== 0){ clearInterval(setButtonInterval); return; }
-            
+
             try {
                 imgBase = $('#watch7-content > link[itemprop="thumbnailUrl"]')[0].href;
                 imgBase = imgBase.split('/');
@@ -143,9 +139,9 @@
                 cl(err);
                 return;
             }
-            
+
             clearInterval(setButtonInterval); //cl(imgSrc);  //yt-uix-button yt-uix-button-hh-text
-            
+
             //imgSrc = imgBase+images[2]; //cl(imgSrc);
             $('<button id=YouThumbButton class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup yt-uix-button-toggled yt-uix-post-anchor yt-uix-tooltip"><span id=YouThumbStatus>'+ t.show_thumb[LANG] +' </span> </button>').appendTo($('.like-button-renderer')[0]);
 
@@ -157,24 +153,25 @@
             $('body').append(images_to_insert);
             getWidths();
         }
-        
+
         function mocallback(mutationrecords){
             if($('title').html() == title || !/watch/i.test(location.href)) return;
             count = 0;
+            defaultImgSrc = 0;
             setButtonInterval = setInterval(SetButton, 500); // TODO launch every 200 msecs. while element link[itemprop="thumbnailUrl"] not be found, and no more than 50 times!
         }
-        
+
         var title = $('title');
-        
+
         if(!isset(title[0])) return;
         var title_content = title.html();
         var mo = new MutationObserver(mocallback);
         var options = {'childList': true};
         mo.observe(title[0], options); //mo.observe(document.title, options); mo.observe(qs('head>title'), options);
         //mo.observe(document.getElementById('eow-title'), options);
-        
+
         $('body').on('click', '.YouThumbButtonImage', ShowHideThumbnail);
         $('body').on('click', '#YouThumbStatus', ShowHideThumbnail);
-            
+
     });
 })(window.jQuery.noConflict(true));
